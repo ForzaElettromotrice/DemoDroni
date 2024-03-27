1b7a60a0debf9a129f5a22fe8d290cfc1439516c
Sharp-p <thomassharp2003@gmail.com>
2024-03-27 10:03:42 +0100

    Icone dei droni che rispecchiano i droni effettivi nel gruppo (è statico, è ancora da mettere nel loop che mantiene sempre aggiornato l'interfaccia)

--- src/interface.py
+++ src/interface.py
@@ -7,7 +7,7 @@ class UI:
     tags = {}
     
     #Crea l'interfaccia
-    def createInterface(self):
+    def createInterface(self, team_droni):
         dpg.create_context()
     #   1.  ancorare finestre       ---FATTO---
     #   2.  sovrascrivere chisura
@@ -18,12 +18,11 @@ class UI:
             with dpg.window(width=500, height=300, tag= "infoDrone", label= "Info drone", modal= True):
                 '''NOTA: il tag dentro ad un evento che può accadere più volte causerà errore dopo la prima volta
                         occorre quindi effettuare una remove_alias() alla fine della funzione'''
-
-                dpg.add_text("Informazione drone")
-            print(sender)
-            print(app_data)
-            print(user_data)
-
+                dpg.add_text("ID: " + str(user_data[0].id))
+                dpg.add_text("Stato: " + str(user_data[0].stato))
+                dpg.add_text("Posizione: " + str(user_data[0].posizione))
+                dpg.add_text("Batteria: " + str(user_data[0].batteria) + "%")
+                dpg.add_text("Obbiettivo: " + str(user_data[0].obbiettivo))
             dpg.remove_alias("infoDrone")
 
         def sideBarFilterCallback(sender, filter):
@@ -31,7 +30,9 @@ class UI:
         
 
         screenSize = (get_monitors()[0].width, get_monitors()[0].height)
-        width, height, channels, data = dpg.load_image("/home/thomas/Documents/Università/DemoDroni/src/Thomas/droneMQ_9.jpg")
+        connected = dpg.load_image("/home/thomas/Documents/Università/DemoDroni/src/dronesImg/droneConnected.png")
+        not_connected = dpg.load_image("/home/thomas/Documents/Università/DemoDroni/src/dronesImg/droneNotConnected.png")
+        not_operational = dpg.load_image("/home/thomas/Documents/Università/DemoDroni/src/dronesImg/droneNotOperational.png")
 
         with dpg.value_registry(label= "Registro valori", tag = "baseValues", user_data= ("valore1", 2, "valore3")):
             dpg.add_string_value(default_value= "Stringa di dafault", tag="defString")
@@ -40,7 +41,10 @@ class UI:
             #non so che cazzo ce va qua (devo trovare delle informazioni su cosa va dentro dpg.theme_component()
 
         with dpg.texture_registry():
-            dpg.add_static_texture( width= width, height= height, default_value= data, tag= "liberta",)
+            dpg.add_static_texture(width= connected[0], height= connected[1], default_value= connected[3], tag= "connected")
+            dpg.add_static_texture(width= not_connected[0], height= not_connected[1], default_value= not_connected[3], tag= "not_connected")
+            dpg.add_static_texture(width= not_operational[0], height= not_operational[1], default_value= not_operational[3], tag= "not_operational")
+
 
         with dpg.window(width=600, height=600, tag= "mapContainer", label= "Mappa", no_title_bar= True, no_move = True, no_resize= True):
             self.tags["maps"] = map.add_map_widget(width= screenSize[0]-200, height= screenSize[1], center= (41.890210, 12.492231), zoom_level= 12)
@@ -49,15 +53,19 @@ class UI:
         with dpg.window( tag="sidebar", label= "Sidebar - Lista droni", pos= (600, 0), no_move = True, no_resize= True, no_title_bar= True):
             dpg.add_input_text(hint= "Cerca droni", callback=sideBarFilterCallback)
             with dpg.filter_set(tag = "sideBarFilter"):
-                for i in range(5):
-                    with dpg.group(label= "Drone", filter_key= "Drone " + str(i+1)):
+                for id, drone in team_droni.ids.items():
+                    with dpg.group(label= "Drone", filter_key= "Drone " + str(id)):
                         with dpg.group(label= "left", width= 80, horizontal= True, horizontal_spacing= 20):
-                            dpg.add_text("Drone "+ str(i+1))
-                            dpg.add_button(callback=info_drone, user_data= ["ciaone", 1, 2, 3, "basta"], label= "Info")
-                        '''with dpg.group(label= "right", width= 80, pos= (100, 100)):
-                            dpg.add_button(callback=info_drone, user_data= ["ciaone", 1, 2, 3, "basta"], label= "Info")'''
+                            dpg.add_text("Drone "+ str(id))
+                            dpg.add_button(callback=info_drone, user_data= [drone], label= "Info")
+
                         with dpg.drawlist(width= 170, height= 170):
-                            dpg.draw_image("liberta", pmin= (0,0), pmax= (179, 179))
+                            if drone.stato == "connesso":
+                                dpg.draw_image("connected", pmin= (0,0), pmax= (170, 170))
+                            elif drone.stato == "sconnesso":
+                                dpg.draw_image("not_connected", pmin= (0,0), pmax= (170, 170))
+                            else:
+                                dpg.draw_image("not_operational", pmin= (0,0), pmax= (170, 170))
 
         dpg.configure_item(item= "sidebar", width= 200, height= 600)
 
--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 from serverSocket.server import ServerSocket
 from interface import UI as droneUI
+from dronesHandler import TeamDroni
+
 
 if __name__ == '__main__':
     server = ServerSocket()
     server.start()
     input("Premi qualcosa per chiudere...\n")
     server.stop()
+    team_droni = TeamDroni()
+    for i in range(7):
+        team_droni.update_drone(i, "connesso", (100*i, 100*i), 50, (50*i, 50*i))
+    team_droni.ids[3].stato = "sconnesso"
+    team_droni.ids[6].stato = "non operativo"
+    team_droni.ids[5].stato = "non operativo"
+
     droneTestUI = droneUI()
-    droneTestUI.createInterface()
+    droneTestUI.createInterface(team_droni)
